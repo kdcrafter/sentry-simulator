a0e523892376f41074ad52a0009075e89c499cdf
Kurtis Dressel <kurtisdoubled@hotmail.com>
2020-06-18 23:58:32 -0500

    added top down camera and set exact locations

--- sketch.js
+++ sketch.js
@@ -1,36 +1,49 @@
+// Note: coordinate position units are in mm
+
+// Global variables
+let cam, sentryCam;
+
 function setup() {
     createCanvas(700, 700, WEBGL);
+
+    // Cameras
+    let eyeZ = (height/2.0) / tan(PI * 60.0/360.0);
+
+    topCam = createCamera();
+    topCam.setPosition(0, -3500, -2500.0001); // camera can't look stright up or down ?
+    topCam.lookAt(0, 1300, -2500);
+    topCam.perspective(PI/3.0, width/height, eyeZ/10.0, eyeZ*15.0);
+
+    sentryCam = createCamera();
+    sentryCam.setPosition(0, 0, 0);
+    sentryCam.perspective(PI/3.0, width/height, eyeZ/10.0, eyeZ*15.0);
+
+    setCamera(sentryCam);
 }
 
 function draw() {
+    // Settings
     rectMode(CENTER);
 
+    // Scene
     background(150);
     ambientLight(255);
 
-    // let camPos = createVector(0, 0, 0);
-    // let camCenter = createVector(0, 1, 0);
-    // let camUp = createVector(0, 1, 0);
-    // camera(camPos, camCenter, camUp);
-
-    cam = createCamera();
-    cam.setPosition(0, -200, 0.00001); // a z value of 0 causes canvas to only show background
-    cam.lookAt(0, 0, 0);
-
+    // Ground
     push();
-    rotateX(millis() / 1000);
-    rotateY(millis() / 1000);
-    rotateZ(millis() / 1000);
-
-    ambientMaterial(255);
-    sphere();
-    pop();
-
-    push();
-    translate(0, 100, 0);
+    translate(0, 1300, -2500);
     rotateX(HALF_PI)
 
     ambientMaterial(100);
-    plane(10000, 10000);
+    plane(20000, 20000);
+    pop();
+
+    // Objects
+    push();
+    translate(0, 0, millis()/5 - 2500);
+
+    noStroke();
+    ambientMaterial(255);
+    sphere(17);
     pop();
 }
